42d2729b59c237d38637de5a6873a83eaeba7fa7
Dragos Sebestin <sebestin.dragos@gmail.com>
2018-12-10 02:05:36 +0200

    [master] Implemented poll result widget.

--- src/client/core/debates/debate.service.ts
+++ src/client/core/debates/debate.service.ts
@@ -4,6 +4,7 @@ import EPollAPI from 'epoll-api-sdk';
 
 // types
 import { IDebatePollListItem, IDebate } from '../../types/debates/IDebate';
+import { IVote } from '../../types/debates/IVote';
 
 /**
  * Angular Debate service class.
@@ -40,5 +41,18 @@ export class DebateService {
    */
   getPollById (id: string) : Observable<IDebate<any>> {
     return EPollAPI.Debates().getDebateById(id);
+  }
+
+  /**
+   * Add vote to poll.
+   */
+  voteOnPoll (params: {
+    pollId: string,
+    selectedOptionId: string
+  }) : Observable<IVote> {
+    return EPollAPI.Debates().addPollVote({
+      pollId: params.pollId,
+      optionId: params.selectedOptionId
+    });
   }
 }
--- src/client/core/debates/renderer/components/poll-debate.component.ts
+++ src/client/core/debates/renderer/components/poll-debate.component.ts
@@ -1,12 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import moment from 'moment';
 
 import { IDebateComponent } from './IDebateComponent';
+import { ENVIRONMENT_CONFIG, IEnvironmentConfig } from '../../../../environment.config';
 
 // types
 import { IDebate, IPollDebate } from '../../../../types/debates/IDebate';
-import { IAttachment } from '../../../../types/debates/IAttachment';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { DebateService } from '../../debate.service';
+import { IVote } from '../../../../types/debates/IVote';
+
+export interface IPollResult {
+  name: string,
+  count: number
+}
+
+const SHARE_HASHTAG = '#CraiovaPD';
 
 /**
  * Component used for rendering a poll debate's
@@ -24,11 +33,17 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class PollDebateComponent implements IDebateComponent, OnInit {
   public debate: IDebate<IPollDebate> | undefined;
   public voteForm: FormGroup;
+  public hasNotVoted = false;
+  public pollResults: IPollResult[] = [];
 
   /**
    * Class constructor.
    */
-  constructor (formBuilder: FormBuilder) {
+  constructor (
+    private _debates: DebateService,
+    @Inject(ENVIRONMENT_CONFIG) private _env: IEnvironmentConfig,
+    formBuilder: FormBuilder
+  ) {
     this.voteForm = formBuilder.group({
       voteOptionId: ['', Validators.required]
     });
@@ -44,6 +59,20 @@ export class PollDebateComponent implements IDebateComponent, OnInit {
    */
   setDebate (debate: IDebate<any>): void {
     this.debate = debate;
+
+    // check if user has already voted
+
+    // compute poll results
+    this.pollResults = this.debate.payload.options.map(option => {
+      let count = debate.payload.votes.data.reduce((sum: number, crt: IVote) => {
+        return sum + (crt.optionId === option._id ? 1 : 0);
+      }, 0);
+
+      return {
+        name: option.reason,
+        count
+      };
+    });
   }
 
   /**
@@ -55,9 +84,44 @@ export class PollDebateComponent implements IDebateComponent, OnInit {
   }
 
   /**
-   * Download an attachment on the device.
+   * Add a vote on this poll.
    */
-  downloadAttachment (attachment: IAttachment) {
-    attachment.file.originalName;
+  async vote (debate: IDebate<any>, optionId: string) {
+    try {
+      await this._debates.voteOnPoll({
+        pollId: debate._id,
+        selectedOptionId: optionId
+      });
+    } catch (error) {
+
+    }
+  }
+
+  /**
+   * Encode an URI component.
+   */
+  encodeWhatsappMessage (debate: IDebate<any>) : string {
+    return encodeURIComponent(
+`
+${debate.payload.title}
+
+${window.location.href}
+`
+    );
+  }
+
+  /**
+   * Share page on Facebok.
+   */
+  facebookShare (debate: IDebate<any>) {
+    if (this.debate) {
+      let pageUrl = encodeURIComponent(window.location.href);
+      let shareQuote = encodeURIComponent(debate.payload.title);
+      let shareTag = encodeURIComponent(SHARE_HASHTAG);
+      /* tslint:disable */
+      let shareUrl = `https://www.facebook.com/dialog/share?app_id=${this._env.facebook.appId}&display=popup&href=${pageUrl}&quote=${shareQuote}&hashtag=${shareTag}`;
+      /* tslint:enable */
+      window.open(shareUrl, '', 'height=300,width=600');
+    }
   }
 }
