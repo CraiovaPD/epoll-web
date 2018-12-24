15748a98a61139495dabd183b5fdcfeffae68c85
Dragos Sebestin <sebestin.dragos@gmail.com>
2018-12-24 17:26:33 +0200

    [master] Change site title and description when viewing a debate.

--- src/client/pages/debate-details/page.component.ts
+++ src/client/pages/debate-details/page.component.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Title, Meta } from '@angular/platform-browser';
 
 // types
 import { IDebate } from '../../types/debates/IDebate';
@@ -20,15 +21,45 @@ export interface IPageData {
     'page.component.css'
   ]
 })
-export class DebateDetailsPageComponent {
+export class DebateDetailsPageComponent implements OnInit, OnDestroy {
+  private _originalTitle: string = '';
+  private _originalDescription: string = '';
+
   public debate: IDebate<any>;
 
   /**
    * Class constructor.
    */
-  constructor (activatedRoute: ActivatedRoute) {
+  constructor (
+    private _title: Title,
+    private _meta: Meta,
+    activatedRoute: ActivatedRoute
+  ) {
     let data = activatedRoute.snapshot.data.pageData as IPageData;
 
     this.debate = data.debate;
+  }
+
+  /**
+   * Angular lifecycle hooks.
+   */
+  ngOnInit () {
+    this._originalTitle = this._title.getTitle();
+    this._title.setTitle(this.debate.payload.title);
+
+    let foundTag = this._meta.getTag('description');
+    if (foundTag) {
+      this._originalDescription = foundTag.content;
+    }
+    this._meta.updateTag({
+      name: 'description', content: this.debate.payload.content
+    }, `name='description'`);
+  }
+
+  ngOnDestroy () {
+    this._title.setTitle(this._originalTitle);
+    this._meta.updateTag({
+      name: 'description', content: this._originalDescription
+    }, `name='description'`);
   }
 }
