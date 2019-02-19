9f7cf76172e2795968340b8f85a71be080530ab6
Dragos Sebestin <sebestin.dragos@gmail.com>
2019-02-19 18:00:53 +0200

    [master] Added Open Graph and JSON/LD info to home page.

--- src/client/pages/home/page.component.ts
+++ src/client/pages/home/page.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectionStrategy, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformServer } from '@angular/common';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
 
+import { ENVIRONMENT_CONFIG, IEnvironmentConfig } from '../../environment.config';
 import { IAppState } from '../../store/IApp';
 import { ErrorUtil } from '../../util/helpers/errorUtil';
 import { DebateService } from '../../core/debates/debate.service';
@@ -10,6 +12,7 @@ import { DebateService } from '../../core/debates/debate.service';
 // types
 import { IDebatePollListItem, DebateState, IDebateAnouncementListItem } from '../../types/debates/IDebate';
 import { UserRole } from '../../types/users/IUser';
+import { Title, Meta } from '@angular/platform-browser';
 
 /**
  * Component used for displaying the home page.
@@ -25,6 +28,7 @@ import { UserRole } from '../../types/users/IUser';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomePageComponent implements OnInit, OnDestroy {
+  public SCHEMA = {};
   public polls$: Observable<IDebatePollListItem[]>;
   public anouncements$: Observable<IDebateAnouncementListItem[]>;
   public isAdmin$: Observable<boolean>;
@@ -35,7 +39,11 @@ export class HomePageComponent implements OnInit, OnDestroy {
   constructor (
     private _errors: ErrorUtil,
     private _store: Store<IAppState>,
-    private _debateService: DebateService
+    private _debateService: DebateService,
+    @Inject(PLATFORM_ID) private _platformId: Object,
+    @Inject(ENVIRONMENT_CONFIG) private _env: IEnvironmentConfig,
+    private _title: Title,
+    private _meta: Meta
   ) {
     this.isAdmin$ = this._store.select(s => s.profile)
     .pipe(map(profile => {
@@ -63,12 +71,45 @@ export class HomePageComponent implements OnInit, OnDestroy {
    */
   async ngOnInit () {
     try {
+      if (isPlatformServer(this._platformId)) {
+        let descriptionTag = this._meta.getTag('description');
+        this._meta.addTags([{
+          property: 'og:title',
+          content: this._title.getTitle()
+        }, {
+          property: 'og:description',
+          content: descriptionTag ? descriptionTag.content : 'Ţinem cont de vocea cetăţenilor din Craiova'
+        }, {
+          property: 'og:type',
+          content: 'website'
+        }, {
+          property: 'og:url',
+          content: this._env.baseUrl
+        }, {
+          property: 'og:image',
+          content: this._getLogoURL()
+        }]);
 
+        this.SCHEMA = {
+          '@context': 'https://schema.org',
+          '@type': 'WebSite',
+          'image': this._getLogoURL(),
+          'name': this._title.getTitle(),
+          'url': this._env.baseUrl
+        };
+      }
     } catch (err) {
       this._errors.dispatch(err);
     }
   }
 
   ngOnDestroy () {
+  }
+
+  /**
+   * Get logo URL for meta tags.
+   */
+  private _getLogoURL () : string {
+    return `${this._env.baseUrl}assets/logo-mono.png`;
   }
 }
